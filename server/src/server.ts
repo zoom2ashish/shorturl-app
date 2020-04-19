e023e51bba568882f6684315c7882a05a2b27152
Ashish Patel <ashishp@vmware.com>
2020-04-19 14:07:35 -0700

    first commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,17 @@ import * as methodOverride from 'method-override';
 import { resolve, join } from 'path';
 import { UrlRedirectionController } from './controllers/url-redirection/url-redirection.controller';
 import * as serveStatic from 'serve-static';
+import { $log } from '@tsed/common';
+import * as dotEnv from 'dotenv';
+
+const envConfig = dotEnv.config();
+
+console.log('PORT=', process.env.PORT);
+console.log('MONGOOSE_URL=', process.env.MONGOOSE_URL);
 
 const rootDir = resolve(__dirname);
-const UI_PATH = join(rootDir, 'webapp');
-
+const UI_PATH = join(rootDir, '../../client/build');
+console.log('UI_PATH=', UI_PATH);
 @ServerSettings({
   rootDir,
   port: process.env.PORT || 5000,
@@ -21,7 +28,7 @@ const UI_PATH = join(rootDir, 'webapp');
   mongoose: {
     urls: {
       default: {
-        url: 'mongodb+srv://demo:demo@mean-udemy-cluster-pshfy.mongodb.net/shorturl-app-db?retryWrites=true&w=majority',
+        url: process.env.MONGOOSE_URL,
         connectionOptions: {}
       }
     }
@@ -30,6 +37,7 @@ const UI_PATH = join(rootDir, 'webapp');
 export class Server extends ServerLoader {
 
   public $beforeRoutesInit(): void | Promise<any> {
+    $log.info('UI_PATH=', UI_PATH);
     this
       .use(GlobalAcceptMimesMiddleware)
       .use(serveStatic(UI_PATH, {}))
