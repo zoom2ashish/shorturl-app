fbadbd46fb91f5490099677beec1ebb47b361407
Ashish Patel <ashishp@vmware.com>
2020-04-26 15:30:54 -0700

    server-tsed removed and packages removed from package.json

--- server-tsed/src/models/url-management.model.ts
+++
@@ -1,3 +0,0 @@
-export interface CreateShortUrlPayload {
-  url: string;
-}
--- server-tsed/src/utils/url-shortner.util.ts
+++
@@ -1,7 +0,0 @@
-const Hashids = require("hashids/cjs");
-const HASH_ALPHABETS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-";
-
-export function encodeUrl(url: string): string {
-    const hashids = new Hashids(url, 6, HASH_ALPHABETS);
-    return hashids.encode([1, 2, 3, 4, 5, 6]);
-  }
